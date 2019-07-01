f222a93e82ad5e2efca9bedef9a8ddb002d5f1d2
1176547971 <1176547971@qq.com>
2019-07-01 11:17:20 +0800

    111

--- src/main/java/com/wx/controller/OrderController.java
+++ src/main/java/com/wx/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.wx.controller;
 
 import com.wx.service.OrdersService;
+import com.wx.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,4 +11,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OrderController {
 @Autowired
     private OrdersService ordersService;
+    private ProductService productService;
 }
